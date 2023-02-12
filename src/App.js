c3579a64b600b60f0ad6d2b690f64e590e596772
Hamza Burhan <hamzaburhan0985@gmail.com>
2023-02-12 10:01:53 +0000

    Header.jsx divider

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ import { menu } from "./menu";
 import CustomerManagement from "./scenes/CustomerManagement/CustomerManagement";
 import BusinessManagement from "./scenes/BusinessManagement/BusinessManagemnet";
 import MembershipPlan from "./scenes/managementPlan/managementPlan";
+import Divider from "@mui/material/Divider";
+
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
@@ -37,33 +39,40 @@ function App() {
 
   return (
     // <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Topbar setIsSidebar={setIsSidebar} />
-        <div className="app">
-          <Sidebar isSidebar={isSidebar} />
-          <Box sx={{ flexGrow: 1, flexDirection: "column" }}>
-            <Header title={headerTitle} />
-            <main className="content">
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/customermanagement" element={<CustomerManagement />} />
-                <Route path="/businessmanagement" element={<BusinessManagement />} />
-                <Route path="/membershipPlan" element={<MembershipPlan />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/line" element={<Line />} />
-                <Route path="/faq" element={<FAQ />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/geography" element={<Geography />} />
-              </Routes>
-            </main>
-            <Footer />
-          </Box>
-        </div>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Topbar setIsSidebar={setIsSidebar} />
+      <div className="app">
+        <Sidebar isSidebar={isSidebar} />
+        <Box sx={{ flexGrow: 1, flexDirection: "column" }}>
+          <Header title={headerTitle} />
+          
+          <main className="content">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route
+                path="/customermanagement"
+                element={<CustomerManagement />}
+              />
+              <Route
+                path="/businessmanagement"
+                element={<BusinessManagement />}
+              />
+              <Route path="/membershipPlan" element={<MembershipPlan />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+          <Footer />
+        </Box>
+      </div>
+    </ThemeProvider>
     // </ColorModeContext.Provider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,40 @@
 import { Typography, Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import AppBar from "@mui/material/AppBar";
-import Divider from '@mui/material/Divider';
+import Divider from "@mui/material/Divider";
 const Header = ({ title }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
     <>
-      <AppBar position="relative" sx={{backgroundColor:"white", height:"45px", p:"10px 0 0 10px"}}>
+      <AppBar
+        position="relative"
+        sx={{
+          backgroundColor: "white",
+          height: "45px",
+          p: "10px 0 0 10px",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
         <Typography
           variant="h5"
           color={colors.grey[700]}
-          sx={{fontWeight:"bold"}}
+          sx={{ fontWeight: "bold" }}
         >
           {title}
         </Typography>
+
+        <Divider
+          orientation="vertical"
+          sx={{
+            fontWeight: "bold",
+            ml: "20px",
+            backgroundColor: "gray",
+            height: "25px",
+          }}
+          flexItem
+        ></Divider>
       </AppBar>
     </>
   );
